3a0692c817f7ccfb993434d2fb36fd9d395be0e0
akerue <hsmgn8118akerue@gmail.com>
2014-11-10 02:26:42 +0900

    Enable to use twitter bootstrap

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 App = Ember.Application.create();
 
+App.ApplicationController = Ember.ObjectController.extend({
+    actions: {
+      front: function() {
+        console.debug("front");
+      },
+    right: function() {
+        console.debug("right");
+      },
+    left: function() {
+        console.debug("left");
+      },
+    back: function() {
+        console.debug("back");
+      }
+    }
+});
+
